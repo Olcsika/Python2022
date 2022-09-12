469a6f597f9f81fbba96f175f3268071d6617c74
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-09-12 13:17:58 +0200

    yeeeee

---
+++ valtozok.py
@@ -0,0 +1,19 @@
+x=12
+y=13
+ember="Lajoska"
+
+print(x)
+print(x+y)
+
+print(ember)
+
+ember="Jaloska"
+print(ember + " " + str(x),sep="---------")
+ember=42
+print(str (ember) + "*" + str(x)+ "="+ str(ember*x))
+
+x=x+1
+print(x)
+
+
+
