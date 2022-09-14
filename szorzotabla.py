12ce184051519653d1ece2f3df050d8dfadd3cf2
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-09-14 13:32:20 +0200

    yeeee

---
+++ szorzotabla.py
@@ -0,0 +1,14 @@
+
+x=10
+szo="Tizes szorzotabla"
+print(szo)
+print (str (x) + "*" + str(1)+ "="+ str(x*1)) 
+print (str (x) + "*" + str(2)+ "="+ str(x*2))
+print (str (x) + "*" + str(3)+ "="+ str(x*3))
+print (str (x) + "*" + str(4)+ "="+ str(x*4))
+print (str (x) + "*" + str(5)+ "="+ str(x*5))
+print (str (x) + "*" + str(6)+ "="+ str(x*6))
+print (str (x) + "*" + str(7)+ "="+ str(x*7))
+print (str (x) + "*" + str(8)+ "="+ str(x*8))
+print (str (x) + "*" + str(9)+ "="+ str(x*9))
+print (str (x) + "*" + str(10)+ "="+ str(x*10))
