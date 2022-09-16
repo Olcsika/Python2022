9eb8a0fa0b9f54c8a9a9a35ea7fcd64cc914a202
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-09-16 08:45:07 +0200

    yeeeeeeeeee

---
+++ lista.py
@@ -0,0 +1,12 @@
+lista=["alma","körte","kutya",42]
+print(lista)
+print(lista[0])
+print(len(lista))
+print(lista[len(lista) -1])
+print(type(lista[len(lista) -2]))
+lista[2]="cukornád"
+print(lista)
+lista.append("tomi")
+print(lista) 
+lista.insert(1,"csocsi")
+print(lista)
