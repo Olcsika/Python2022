c09c879cc82e89f5ec191f7081155df02407e0ea
Én <98027672+Olcsika@users.noreply.github.com>
2022-10-09 18:06:50 +0200

    Add files via upload

---
+++ csoprtm.1.py
@@ -0,0 +1,9 @@
+print("Sebesség kiszámítás (v)")
+a=int(input("(s) Kérem a meg tett utat: "))
+b=int(input("(t) Kérem az eltelt időt: "))      
+
+sebbesseg=a/b
+print("Képlet:")
+print("\t- sebesség = út/idő")
+
+print("Eredmény:",sebbesseg)
