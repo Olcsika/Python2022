beffe286c20930d02494883d5f40ae6e7dfddb20
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-10-28 08:06:14 +0200

    dasadasdas

--- csoprtm.1.py
+++ csoprtm.1.py
@@ -1,6 +1,12 @@
-print("Sebesség kiszámítás (v)")
-a=int(input("(s) Kérem a meg tett utat: "))
-b=int(input("(t) Kérem az eltelt időt: "))      
+
+
+a=int(input("(s) Kérem a megtett utat: "))
+b=int(input("(t) Kérem az eltelt időt: "))
+c=int(input("(t) Kérem a sebességet: "))
+muvelet=input("Add meg mit szeretnél kiszámolni: ")
+
+if muvelet== sebesség
+      print (int(a+b))
 
 sebbesseg=a/b
 print("Képlet:")
