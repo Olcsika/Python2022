3e63c5c41573e8cfb128454559cb739fe4f41c67
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 14:04:16 +0100

    dwedq

--- szamBe.py
+++ szamBe.py
@@ -4,11 +4,25 @@
 
 
 def szamBe(szoveg,tort=False,minimum=False,maximum=False):
-    print(szoveg)
-    print(tort)
+#    print(szoveg)
+#    print(tort)
+#    print(minimum)
+    hiba=True
+    while hiba:
+        try:
+            if tort:
+                szam=float(input(szoveg))
+              
+            else:
+                szam=int(input(szoveg))
+        except:
+            print("HIBÁS TE GYÁSZ!!")
+        else:
+            hiba= False
 
-szamBe("Aggyyá meg számót: ",tort=True)
 
-print("dawdadaawdadwadad",end="")
-print("12222222")
+szamBe("Aggyyá meg számót: ",minimum=10,tort=True)
+
+#print("dawdadaawdadwadad",end="")
+#print("12222222")
 
