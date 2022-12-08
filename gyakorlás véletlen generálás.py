990efc71dce521f318764a5c716d20c468afe13f
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-12-08 13:18:28 +0100

    s

--- gyakorlás véletlen generálás.py
+++ gyakorlás véletlen generálás.py
@@ -2,7 +2,6 @@ import random
 import math
 
 l=[]
-
 for i in range(10):
     szam=random.random()
     l.append(math.floor(szam*90)+10)
@@ -14,4 +13,39 @@ l=[]
 for i in range(10):
     
     l.append(random.randint(10,99))
+    
 print(l)
+
+
+print(random.randint(-1000,1000)*3)
+l=[]
+for i in range(100):
+    l.append(random.randint(-1000,1000)*3)
+#print(l)    
+
+l5=[]
+
+print(sum(l))
+
+for e in l:
+   if e%5==0:
+      l5.append(e)
+
+print(l5)
+
+l5=[e//15 for e in l if e %5==0]
+
+print(l5)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- masodfok.py
+++ masodfok.py
@@ -1,7 +1,22 @@
 import math
 
-#a*X2+b*x+c
+def egyenlet(a,b,c):
+   szoveg="0 = "
+   if a!=0:
+      szoveg+=str(a)+"x"
 
+   if b>0:
+      szoveg+="+"+str(b)+"x"
+   elif b<0:
+      szoveg+=""+str(b)+"x"
+   if c>0:
+      szoveg+="+"+str(c)
+   elif c<0:
+      szoveg+=""+str(c)
+   return szoveg   
+      
+
+      
 a = int(input("a="))
 
 b = int(input("b="))
@@ -10,6 +25,21 @@ c = int(input("c="))
 
 #(-b+-gyök(b2-4ac))/2a
 
-math.sqrt
+#print(math.sqrt(3))
+
+diszkriminans=b*b-4*a*c
+
+if diaszkriminans<0:
+   print("Nincs megoldás")
+elif diszkriminans==0:
+   megoldas=-b / (2*a)
+   print("1 megoldás:{}".format(megoldas))
+else:
+   x1=(-b+math.sqrt(diszkriminans)) / (2*a)
+   x2=(-b-math.sqrt(diszkriminans)) / (2*a)
+   print("2 megoldás: x1:{},x2:{}".format(x1,x2))
+
+#gyok=mathsqrt(1)
+print(egynelet(a,b,c))   
 
 
