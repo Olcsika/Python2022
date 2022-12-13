2bc74e08c0bf4ae3cd111fc23d3b14a6ae5d2838
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2022-12-13 14:47:27 +0100

    s

--- random szavak.py
+++ random szavak.py
@@ -7,3 +7,17 @@ szavak=["alma","körte","barack","banón","dinnye","szőlő"]
 print( szavak[random.randint(0,len(szavak)-1)])
 
 print(random.choice(szavak))
+
+nagylista=[]
+for e in szavak:
+    #print("-"*20)
+    kislista=[]
+    kislista.append(e)
+    kislista.append(random.randint(12,312))
+    #print(kislista)
+    nagylista.append(kislista)
+print(nagylista)        
+
+
+for e in nagylista:
+    print(e[0].rjust(10),str(e[1]).rjust(4),"kg")
