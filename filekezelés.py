6ad49bc2900545b8d9c60f67635210ac146f2930
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-01-12 13:18:41 +0100

    a

--- filekezelés.py
+++ filekezelés.py
@@ -1,9 +1,27 @@
 #file kezelés
 
 f=open("proba.txt","w")
-
 f.write("HELLO")
+f.write("\n")
 f.write("kutya")
 
 f.close()
 
+f=open("proba.txt","r")
+
+szoveg=(f.read())
+
+sorok=szoveg.split("\n")
+print(sorok)
+
+
+f.close()
+#file tartalma soronként 2
+f=open("proba.txt","r")
+sorok=[]
+for sor in f:
+    sorok.append(sor.replace("\n","")).replace("\r",""))
+
+print(sorok)    
+
+f.close()
