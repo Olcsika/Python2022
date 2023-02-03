4a150e6083f7243fd75c3789252812c2b3018bb1
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-02-03 09:39:30 +0100

    a

--- érettttségi feladatok/Számok/szamok.py
+++ érettttségi feladatok/Számok/szamok.py
@@ -1,4 +1,4 @@
-
+import random
 
 f=open("felszam.txt","r")
 
@@ -33,3 +33,62 @@ for e in kerdesek:
 
 valaszok=[e[1] for e in kerdesek]
 print("a válaszok számértéke {}-től {}-ig tart.".format(min(valaszok),max(valaszok)))
+
+temakorok=[]
+for e in kerdesek:
+    if e[3] not in temakorok:
+        temakorok.append(e[3])
+
+print()
+print("5.Feladat")  
+print("A témakörök: " + ", ".join(temakorok))
+    
+print("6. Feladat")
+
+
+beker=input("Milyen Témakörből szeretne kérdést kapni? ")
+ujLista=[e for e in kerdesek if e[3]== beker]
+#print(ujLista)
+
+sorsolt=random.choice(ujLista)
+valasz=input(sorsolt[0])
+if int(valasz)== sorsolt[1]:
+    print(str(sorsolt[2]) + " pont")
+else:
+    print("0 pont"  + str(sorsolt[1]) + "pont")
+    print("A helyes válasz: "+str(sorsolt[1]))
+
+print("7.feladat")
+
+
+lista10=[]
+for i in range(10):
+    r=random.choice(kerdesek)
+    while r in lista10:
+        r=random.choice(kerdesek)
+
+    lista10.append(r)
+
+print(lista10)
+
+random.shuffle(kerdesek)
+lista10=kerdesek[0:10]
+#print(len(lista10))
+
+f=open("tesztfel.txt","w")
+
+ossz=0
+for e in lista10:
+    f.write(str(e[2])+" "+str(e[1])+" "+str(e[0])+"\n")
+    ossz+=e[2]
+    
+f.write("A feladatsorra összesen {0} pont adható.".format(ossz))
+f.close
+
+
+
+        
+
+    
+
+
