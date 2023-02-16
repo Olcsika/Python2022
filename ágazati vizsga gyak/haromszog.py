ddf0d9bff7bc805de6d7705419287976babc32f4
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-02-16 13:20:40 +0100

    a

---
+++ ágazati vizsga gyak/haromszog.py
@@ -0,0 +1,14 @@
+def haromszog():
+
+   vissza=[]  
+   for i in range(3)
+      szam="":
+      while szam=="":
+         try:
+            szam=int(input("Kérek egy egész számot: "))
+         except:
+            print("Ez nem egész szám!")
+
+      vissza.append(szam)
+   return vissza
+
