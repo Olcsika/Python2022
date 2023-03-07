04de14db33f7f1daad0e4d74c6676477467becc4
Lébár Olivér <lebaro21@ADY-NAGYATAD.SULINET.HU>
2023-03-07 11:30:11 +0100

    A

--- alap dolgok/gyakorlás alap dolgok.py
+++ alap dolgok/gyakorlás alap dolgok.py
@@ -22,9 +22,81 @@ gyumolcsok[index]+="kos pite"
 print(gyumolcsok[3])
 
 print("Rövid gyümölcsök")
+
+#for i in range(len(gyumolcsok)):
+#      if len(gyumolcsok[i])<5:
+#        print(gyumolcsok[i])
+
+rovid=[]
 for i in range(len(gyumolcsok)):
     if len(gyumolcsok[i])<5:
-        print(gyumolcsok[i])
+        rovid.append(gyumolcsok[i])
+
+print(rovid)
+
+
+rovid=[]
+for e in gyumolcsok:
+    if len(e)<5:
+        rovid.append(e)
+
+print(rovid)
+
+rovid=[e for e in gyumolcsok if len(e)<5]
+
+print(rovid)
+
+rovid=[]
+i=0
+while i<len(gyumolcsok):
+    if len(gyumolcsok[i])<5:
+        rovid.append(gyumolcsok[i])
+    i+=1
+
+print(rovid)
+
+
+
+
+rovid=[]
+i=0
+while True:
+    print(i)
+    if len(gyumolcsok[i])<5:
+        rovid.append(gyumolcsok[i])
+    i+=1
+    if len(gyumolcsok)-1==i:
+        break
+    
+
+print(gyumolcsok[:2])
+        
+print(gyumolcsok[len(gyumolcsok)-2:])
+
+    
+       
+print(gyumolcsok[1:-1])
+
+paratlan=gyumolcsok[1::2]
+
+print(paratlan)
+
+paros=gyumolcsok[0::2]
+
+print(paros)
+
+masolat=gyumolcsok
+masolat.reverse()
+print(", ".join(gyumolcsok[::-1]))
+
+
+
+
+
+
+
+
+
 
 
 
